0c25da5dbe2f4fe268959798125f5f5c1fd47b9d
euan jones <euandmj97@gmail.com>
2020-05-15 23:09:46 +0100

    exmaple stuff

--- Program.cs
+++ Program.cs
@@ -11,50 +11,7 @@ namespace ClassTracker
     {
         static void Main(string[] args)
         {
-            var apple1 = new Apple();
-            var apple2 = new Apple()
-            {
-                skinColor = Color.Green,
-                
-            };
-
-            apple2.size = new Size(69, 69);
-            //var l = apple2.CheckChanged();
-
-            apple2.AddTo(apple1);
-        }
-    }
-
-    public class Apple
-    {
-        public ClassTracker<Apple> PropertyList { get; }     
-
-        public Color skinColor { get; set; }
-        public Size size { get; set; }
-        
-        public int weight { get; set; }
-
-        private int age;
-
-        public Apple()
-        {
-            skinColor = Color.Red;
-            size = new Size(1, 1);
-            weight = 35;
-
-            PropertyList = new ClassTracker<Apple>();
-
-            PropertyList.Register(this);
-        }
-
-        public void AddTo(Apple b)
-        {
-            PropertyList.AddTo(this, b);
-        }
-
-        public IEnumerable<(string name, object value)> CheckChanged()
-        {
-            return PropertyList.CheckChanged(this);
+            Examples.Example01.DoExample();
         }
     }
 }
